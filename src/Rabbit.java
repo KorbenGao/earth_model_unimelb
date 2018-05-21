e4cce0cefe506bb476ad151483c2bde0ee3d5d92
gy <yuang5@student.unimelb.edu.au>
2018-05-21 17:56:44 +1000

    add extension rabbit

--- src/Rabbit.java
+++ src/Rabbit.java
@@ -1,51 +1,60 @@
 
 public class Rabbit {
 
-	// specifies daisy's age
-	private int age;
+    // specifies daisy's age
+    private int age = 0;
+    private int food = 5;
 
-	private int food = 5;
 
-	/**
-	 * increase the daisy's age by 1
-	 */
-	public void age() {
-		food = food - 2;
-		if (food > 0) {
-			age = age + 1;
-		} else {
-			age = age + 6;
-		}
+    public Rabbit() {
+        this.age = 0;
+        this.food = (int) (5 * Math.random());
+    }
 
-	}
+    /**
+     * increase the daisy's age by 1
+     */
+    public void age() {
+        //System.out.println(this.age);
+        food = food - 1;
+        if (food > 0) {
+            age = age + 1;
+        } else {
+            age = age + 6;
+        }
 
-	/**
-	 * 
-	 * @param foodNum
-	 */
-	public void eat(int foodNum) {
-		food = food + foodNum;
-	}
+    }
 
-	public int getAge() {
-		return age;
-	}
+    /**
+     * @param foodNum
+     */
+    public void eat(int foodNum) {
+        food = food + foodNum;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public int getAge() {
+        return age;
+    }
 
-	public int getFood() {
-		return food;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	public void setFood(int food) {
-		this.food = food;
-	}
+    public int getFood() {
+        return food;
+    }
 
-	public void init() {
-		this.food = 5;
-		this.age = 0;
-	}
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public void init() {
+        this.food = 5;
+        this.age = 0;
+    }
+
+    public void feed(int food) {
+        this.food = this.food + food;
+    }
 
 }
